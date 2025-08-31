9b331c714481a1b515409ddc976bb01d523608ea
Danilo Biella <d.biella@gmail.com>
2025-08-31 09:26:58 +0200

    feat: all optimizer parameters in config

--- train.py
+++ train.py
@@ -26,14 +26,15 @@ def initialize_model(model_name: str):
     return model
 
 
-def setup_optimizer(lr, clip):
+def setup_optimizer(starting_lr: float, decay_steps: int, alpha: float, clip: float):
     print("-" * 30)
     print("Setting up optimizer")
-    print(f"Optimizer learning rate: {lr:.2e}")
+    print(f"Optimizer starting learning rate: {starting_lr:.2e}")
+    print(f"Optimizer final learning rate: {starting_lr * alpha:.2e}")
     print(f"Clipping: {clip}")
     print()
     lr_schedule = optax.cosine_decay_schedule(
-        init_value=lr * 10, decay_steps=60_000, alpha=0.1
+        init_value=starting_lr, decay_steps=decay_steps, alpha=alpha
     )
 
     return optax.chain(
@@ -59,14 +60,21 @@ def main(config):
     approximator = func_approx.actor_critic_function_aproximation(
         model, func_approx.into_tensor_representation, game.action_to_num
     )
-    optimizer = setup_optimizer(config["LR"], config["CLIP"])
+    optimizer = setup_optimizer(
+        config["STARTING_LR"], config["DECAY_STEPS"], config["ALPHA"], config["CLIP"]
+    )
     key = jax.random.PRNGKey(config["SEED"])
     loop_fn, init_training_state_fn = actor_critic.get_algo_functions(
         optimizer, approximator, actor_critic_config
     )
 
     training.train(
-        key, loop_fn, init_training_state_fn, approximator, optimizer, training_config
+        key,
+        loop_fn,
+        init_training_state_fn,
+        approximator,
+        optimizer,
+        training_config,
     )
 
 
